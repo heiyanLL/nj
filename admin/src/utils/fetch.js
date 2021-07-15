ec93f3c47d4f21aaad87a3e7c496fc26082cd39b
ghy <312169513@qq.com>
2021-07-15 20:42:10 +0800

    域名更换

--- admin/src/utils/fetch.js
+++ admin/src/utils/fetch.js
@@ -4,7 +4,7 @@ import { Message, Loading } from 'element-ui'
 let loadingInstance = null //这里是loading
 //使用create方法创建axios实例
 export const Service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://121.196.42.224/hy/' : '',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://medical.nanjingdata.cn/' : '',
   timeout: 7000, // 请求超时时间
 })
 // 添加请求拦截器
